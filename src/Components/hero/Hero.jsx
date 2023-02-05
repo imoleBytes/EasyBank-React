66dbc8188359910e08dee39d80af23fe6ca7cb05
imole <dremkay71@gmail.com>
2023-02-05 01:11:21 +0100

    Styles added to the hero section of the desktop

--- src/Components/hero/Hero.jsx
+++ src/Components/hero/Hero.jsx
@@ -8,15 +8,15 @@ const Hero = () => {
         <div className="container">
 
         <div className="hero__img">
-                <img src={mockup} alt=""  />
-            </div>
+            <img src={mockup} alt=""  />
+        </div>
             
-            <div className="hero__text">
-                <h1>Next generation digital banking</h1>
-                <p>Take your financial life online. Your Easybank account will be a one-stop-shop 
-                    for spending, saving, budgeting, investing, and much more.</p>
-                <a href="" className="btn">Request Invite</a>
-            </div>
+        <div className="hero__text">
+            <h1>Next generation digital banking</h1>
+            <p>Take your financial life online. Your Easybank account will be a one-stop-shop 
+                for spending, saving, budgeting, investing, and much more.</p>
+            <a href="" className="btn">Request Invite</a>
+        </div>
             
         </div>
     </section>
